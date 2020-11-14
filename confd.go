3286b1fb73210aaaaa3da4e7737681fb9cbb60b2
Dmitry Litokh <dmitry@MacBook-Pro-Dmitry.local>
2020-11-15 09:16:08 +1000

    deleted internal

--- confd.go
+++ confd.go
@@ -13,7 +13,7 @@ import (
 	"os/exec"
 	"github.com/naoina/toml"
 	"gopkg.in/natefinch/lumberjack.v2"
-	"github.com/ltkh/confd/internal/template"
+	"github.com/ltkh/confd/template"
 )
 
 type Config struct {
